4767f2d8486a13734c7cbab477987bf6fc5a5f3e
Liviu <liviu.panainte@gmail.com>
2024-01-13 17:29:29 +0000

    add tudor player

--- public/js/app/Hydra/States/Login.js
+++ public/js/app/Hydra/States/Login.js
@@ -21,28 +21,35 @@ define(['Hydra/Socket/Client'], function (client) {
                 400,
                 200,
                 "Who are you?",
-                {font: "20px Arial", fill: "#ffffff", align: "center"}
+                { font: "20px Arial", fill: "#ffffff", align: "center" }
             );
 
             var p1 = game.add.text(
                 400,
                 240,
                 "Ubick",
-                {font: "20px Arial", fill: "#ffff00", align: "center"}
+                { font: "20px Arial", fill: "#ffff00", align: "center" }
             );
 
             var p2 = game.add.text(
                 400,
                 280,
                 "LordTheCrow",
-                {font: "20px Arial", fill: "#ff8800", align: "center"}
+                { font: "20px Arial", fill: "#ff8800", align: "center" }
             );
 
             var p3 = game.add.text(
                 400,
                 320,
                 "ChocolateLover",
-                {font: "20px Arial", fill: "#ff4400", align: "center"}
+                { font: "20px Arial", fill: "#ff4400", align: "center" }
+            );
+
+            var p4 = game.add.text(
+                400,
+                360,
+                "Tudor = bananalover",
+                { font: "20px Arial", fill: "#00ff00", align: "center" }
             );
 
             var players = [p1, p2, p3];
@@ -51,7 +58,7 @@ define(['Hydra/Socket/Client'], function (client) {
                 var p = players[i];
                 p.inputEnabled = true;
                 p.events.onInputDown.add(function (element) {
-                    game.player = {username: element.text};
+                    game.player = { username: element.text };
                     game.state.start('Game');
                 }, this);
             }
